74ca4ff74a14e8766cdcfbd29fb747ff34421de8
ajstar212 <jubayerabdullah88@gmail.com>
2020-10-09 09:41:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+  var menuToggle = document.querySelector(".toggle");
+  var menu = document.querySelector(".menu");
+  menuToggle.classList.toggle("active");
+  menu.classList.toggle("active");
+}
+
+/*-------Accordion-------*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
